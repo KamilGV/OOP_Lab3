0cb52f5a1bc5e90965b8980b141c3f8ac2dee1bc
Камил Гильванов <gkamilv@gmail.com>
2020-11-11 11:50:07 +0500

    Добавлены классы Shape, Point, Circle, Square

--- OOP_Lab3/OOP_Lab3.py
+++ OOP_Lab3/OOP_Lab3.py
@@ -1,1 +1,27 @@
+class Shape:
+    def __init__(self, obj='Shape'):
+        print('Конструктор объекта: ', obj)
+    def show(self,obj='Shape'):
+        print('Метод объекта ', obj)
 
+class Point(Shape):
+    def __init__(self, obj='Point'):
+        self.x = 0
+        self.y = 0
+        Shape.__init__(self, obj)
+    def show(self, obj = 'Point'):
+        Shape.show(self, obj)    
+    
+class Circle(Point):
+    def __init__(self, obj='Circle'):
+        self.r = 1
+        Shape.__init__(self, obj)
+    def show(self, obj = 'Circle'):
+        Shape.show(self, obj)
+        
+class Square(Shape):
+    def __init__(self, obj='Square'):
+        a = 4
+        Shape.__init__(self, obj)
+    def show(self,obj='Skuare'):
+        Shape.show(self, obj)
